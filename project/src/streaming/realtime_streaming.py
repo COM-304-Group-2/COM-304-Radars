6533f64bc73df402c7b0d85ff07ec8174299d146
Thomas Kemper <thomas.kemper@epfl.ch>
2025-05-29 12:08:54 +0200

    change

--- project/src/streaming/realtime_streaming.py
+++ project/src/streaming/realtime_streaming.py
@@ -143,7 +143,7 @@ class MyApp(ShowBase):
             to_plot = np.abs(Z_polar)
             to_plot = to_plot
             to_plot /= np.max(to_plot)
-            to_plot = to_plot ** 4
+            to_plot = to_plot ** 8
 
             # Convert map to Detections points
             threshold = 0.01
